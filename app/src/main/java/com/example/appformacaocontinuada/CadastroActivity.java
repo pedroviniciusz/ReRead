0167aaaa2ef2bf92b4d52b4219875dcdc0cc97f3
PedroViniciusz <pedrovdjesus@hotmail.com>
2022-11-05 22:39:53 -0300

    Activity Cadastro e Correções na Login

--- app/src/main/java/com/example/appformacaocontinuada/CadastroActivity.java
+++ app/src/main/java/com/example/appformacaocontinuada/CadastroActivity.java
@@ -6,20 +6,21 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class CadastroActivity extends AppCompatActivity {
 
     private Button btnCadastro, btnEntrar;
-    private EditText editTextEmail, editTextSenha;
+    private EditText editTextNome, editTextEmail, editTextSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_cadastro);
 
-        btnCadastro = findViewById(R.id.btnCadastro);
-        btnEntrar = findViewById(R.id.btnEntrar);
+        editTextNome = findViewById(R.id.editTxtNome);
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextSenha = findViewById(R.id.editTextSenha);
+        btnCadastro = findViewById(R.id.btnCadastro);
+        btnEntrar = findViewById(R.id.btnEntrar);
 
     }
 }
